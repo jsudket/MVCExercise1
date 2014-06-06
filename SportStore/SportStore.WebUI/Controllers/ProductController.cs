6295e2e42434bcc419ac80ff37b77529b67a840d
jsudket <jimsudket>
2014-06-07 00:37:47 +0300

    Skeleton complete

---
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
